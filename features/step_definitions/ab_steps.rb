f8bd1286c72ce752c62f1926eb4bfb7fa1ad7895
ehonlia <hongxin.liang@ericsson.com>
2012-06-04 11:03:41 +0800

    i can call it period

--- features/step_definitions/ab_steps.rb
+++ features/step_definitions/ab_steps.rb
@@ -9,3 +9,11 @@ end
 Then /^the response should contain "([^"]*)"$/ do |expected|
   assert_partial_output(expected, response)
 end
+
+Then /^the response should exactly contain "([^"]*)"$/ do |expected|
+  assert_exact_output(expected, response)
+end
+
+Then /^the response should match \/([^\/]*)\/$/ do |expected|
+  assert_matching_output(expected, response)
+end
